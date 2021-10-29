d74a3e9c218f4c4645b2316e792270e4396da103
Franco Lamberti <francolamberti02@gmail.com>
2021-10-28 21:07:24 -0300

    Creando proyecto 2

---
+++ MigraEmprende/src/test/java/com/Mesa5/Mesa5/Mesa5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Mesa5.Mesa5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mesa5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Proyecto-Final-Egg
-# MigraEmprende
