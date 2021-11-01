07d70799886fef48795e481d7950b6416fee7ac8
Alan Coradi <alanncoradi@gmail.com>
2021-11-01 20:45:28 -0300

    ComentarioService completo + UsuarioRepository fix

--- MigraEmprende/src/main/java/com/MigraEmprende/MigraEmprende/services/ComentarioService.java
+++ MigraEmprende/src/main/java/com/MigraEmprende/MigraEmprende/services/ComentarioService.java
@@ -1,21 +1,24 @@
 package com.MigraEmprende.MigraEmprende.services;
 
 import java.sql.Date;
-// import java.util.List;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.MigraEmprende.MigraEmprende.entities.Comentario;
 import com.MigraEmprende.MigraEmprende.entities.Usuario;
+import com.MigraEmprende.MigraEmprende.repositories.ComentarioRepository;
 
 @Service
 public class ComentarioService {
-	/*
+	
+	// INSTANCIAMOS EL REPOSITORIO
 	@Autowired
 	private ComentarioRepository comentRepository;
-	*/
 	
+	// CREAR COMENTARIO
 	@Transactional
 	public Comentario crearComentario(String titulo, String contenido, String email, Date fecha, Usuario usuario) {
 		
@@ -33,24 +36,82 @@ public class ComentarioService {
 		} catch (Exception e) {
 			System.out.println("No se pudo crear el comentario");
 			throw e;
-		}
-		
+		}	
 	}
 	
-	/* LISTAR TODOS LOS COMENTARIOS
+	// LISTAR TODOS LOS COMENTARIOS
 	@Transactional(readOnly=true)
 	public List<Comentario> listarComentarios() {
-		List<Comentario> listaCentarios;
 		try {
+			List<Comentario> listaComentarios;
 			listaComentarios = comentRepository.findAll();
 			return listaComentarios;
 		} catch (Exception e) {
 			System.out.println("No se pudo listar los comentarios");
 			throw e;
+		}	
+	}
+	
+	// BORRAR COMENTARIO
+	@Transactional
+	public void borrarComentario(String id) {
+		try {
+			Comentario entidad = comentRepository.findById(id).get();
+			comentRepository.delete(entidad);
+		} catch (Exception e) {
+			System.out.println("No se pudo eliminar el cometario");
 		}
-		
+	}
+	
+	// BAJA COMENTARIO
+	public Comentario bajaComentario(String id) {
+		try {
+			Comentario entidad = comentRepository.findById(id).get();
+			entidad.setAlta(false);
+			return comentRepository.save(entidad);
+		} catch (Exception e) {
+			System.out.println("No se pudo dar de baja el comentario");
+			throw e;
+		}
+	}
+	
+	// ALTA COMENTARIO
+	public Comentario altaComentario(String id) {
+		try {
+			Comentario entidad = comentRepository.findById(id).get();
+			entidad.setAlta(true);
+			return comentRepository.save(entidad);
+		} catch (Exception e) {
+			System.out.println("No se pudo dar de alta el comentario");
+			throw e;
+		}
+	}
+	
+	// LISTAR COMENTARIOS POR EMAIL 
+	// FALTA QUERY EN REPOSITORY
+	/*
+	public List<Comentario> listarComentariosId(String email){
+		try {
+			List<Comentario> listaComenatario = comentRepository.
+			return listaComentario;
+		} catch (Exception e) {
+			System.out.println("No se pudo listar los comentarios por id");
+			throw e;
+		}
 	}
 	*/
 	
-	
+	// LISTAR COMENTARIOS POR USUARIO
+	// FALTA QUERY EN REPOSITORY
+	/*
+	public List<Comentario> listarComentariosUsuario(Usuario usuario){
+		try {
+			List<Comentario> listaComentario = comentRepository.
+			return listaComentario;
+		} catch (Exception e) {
+			System.out.println("No se pudo listar los comentarios por Usuario");
+			throw e;
+		}
+	}
+	*/
 }
