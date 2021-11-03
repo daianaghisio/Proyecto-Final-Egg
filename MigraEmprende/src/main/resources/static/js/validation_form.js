e1ad91f1d51bab313e9ad02629d2d1e4c033122f
maurodell <delloliomauro@gmail.com>
2021-11-02 21:36:07 -0300

    Se finaliza el formulario de usuario

--- MigraEmprende/src/main/resources/static/js/validation_form.js
+++ MigraEmprende/src/main/resources/static/js/validation_form.js
@@ -1,44 +1,66 @@
 const formulario = document.getElementById('userform');
+
 const inputs = document.querySelectorAll('#userform input');
 
 const expressions = {
-    username: /^[a-zA-Z0-9\_\-]{4,16}$/, //letras, numeros, guion y guion bajo
-    name: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // letras y espacios, puede llevar acentos
-    lastname: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // letras y espacios, puede llevar acentos
+    user: /^[a-zA-Z0-9\_\-]{4,16}$/, //letras, numeros, guion y guion bajo
+    name: /^[a-zA-ZÀ-ÿ\s]{4,40}$/, // letras y espacios, puede llevar acentos
+    lastname: /^[a-zA-ZÀ-ÿ\s]{4,40}$/, // letras y espacios, puede llevar acentos
     pass: /^.{4,12}$/, // 4 a 12 dígitos
     email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
 }
 
-const validationForm = (e) =>{
+const areas = {
+    user: false,
+    name: false,
+    lastname: false,
+    pass: false,
+    email: false
+}
+
+const validationForm = (e) => {
     switch (e.target.name){
-        case "username":
-            if(expressions.username.test(e.target.value)){
-                document.getElementById('name__group').classList.remove('form_group-error');
-                document.getElementById('name__group').classList.add('form_group-right');
-                document.querySelector('#name_group i').classList.add('bi bi-check-circle-fill');
-                document.querySelector('#name_group i').classList.remove('bi bi-exclamation-diamond-fill');
-                document.querySelector('#name_group .group_errorname').classList.remove('group_errorname-activo');
-            }else{
-                document.getElementById('name__group').classList.add('form_group-error');
-                document.getElementById('name__group').classList.remove('form_group-right');
-                document.querySelector('#name_group i').classList.remove('bi bi-check-circle-fill');
-                document.querySelector('#name_group i').classList.add('bi bi-exclamation-diamond-fill');
-                document.querySelector('#name_group .group_errorname').classList.add('group_errorname-activo');
-            }
+        case "name":
+            validationArea(expressions.name, e.target, 'name');
         break;
 
-        case "userlastname":
-            
+        case "lastname":
+            validationArea(expressions.lastname, e.target, 'lastname');
         break;
         case "user":
-            
+            validationArea(expressions.user, e.target, 'user');
         break;
         case "email":
-            
+            validationArea(expressions.email, e.target, 'email');
         break;
         case "pass":
-            
+            validationArea(expressions.pass, e.target, 'pass');
         break;
+    }
+}
+
+const validationArea = (expression, input, area) => {
+    if(expression.test(input.value)){
+        document.getElementById(`group_${area}`).classList.remove('form_group-error');
+        document.getElementById(`group_${area}`).classList.add('form_group-right');
+        document.querySelector(`#group_${area} .group__errorname`).classList.remove('group__errorname-activo');
+        
+        document.getElementById(`${area}`).classList.remove('fm');
+        document.getElementById(`${area}`).classList.add('fm-right');
+
+        areas[area] = true;
+
+        document.getElementById('msj-error').classList.remove('msj-error-activo');
+
+    }else{
+        document.getElementById(`group_${area}`).classList.add('form_group-error');
+        document.getElementById(`group_${area}`).classList.remove('form_group-right');
+        document.querySelector(`#group_${area} .group__errorname`).classList.add('group__errorname-activo');
+
+        document.getElementById(`${area}`).classList.add('fm');
+        document.getElementById(`${area}`).classList.remove('fm-right');
+
+        areas[area] = false;
     }
 }
 
@@ -49,4 +71,21 @@ inputs.forEach((input) => {
 
 formulario.addEventListener('submit', (e) => {
     e.preventDefault();//quita la funcion del boton
+
+    if(areas.name && areas.lastname && areas.user && areas.email && areas.pass){
+        formulario.reset();
+
+        document.getElementById('mensaje-exito').classList.add('mensaje-exito-activo');
+        setTimeout(() =>{
+            document.getElementById('mensaje-exito').classList.remove('mensaje-exito-activo');
+        }, 4000);
+
+        document.querySelectorAll('.form_group-right').forEach((icono) =>{
+            icono.classList.remove('form_group-right');
+        });
+
+        document.getElementById('msj-error').classList.remove('msj-error-activo');
+    }else{
+        document.getElementById('msj-error').classList.add('msj-error-activo');
+    }
 });
