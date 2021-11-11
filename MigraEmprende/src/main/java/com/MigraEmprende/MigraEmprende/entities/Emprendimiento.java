1ed48132cda70ea04551aedd343cb76618d3bf3a
daianaghisio <daianaghisio@gmail.com>
2021-11-11 20:02:03 -0300

    cambios no significativos

--- MigraEmprende/src/main/java/com/MigraEmprende/MigraEmprende/entities/Emprendimiento.java
+++ MigraEmprende/src/main/java/com/MigraEmprende/MigraEmprende/entities/Emprendimiento.java
@@ -1,11 +1,11 @@
 package com.MigraEmprende.MigraEmprende.entities;
 
-import java.util.List;
+//import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+//import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import org.hibernate.annotations.GenericGenerator;
